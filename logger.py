5f21cdce91bca62fc87c9e882a3f55e533951bb4
Richard Yu <yurichard3839@gmail.com>
2018-02-06 21:48:56 +0800

    Update logger.py.

--- logger.py
+++ logger.py
@@ -8,15 +8,21 @@ import logging.handlers
 def getLogger():
     logger = logging.getLogger('blive')
 
-    streamHandler = logging.StreamHandler()
-    streamHandler.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
+    logger.addHandler(stream_handler)
 
-    syslogHandler = logging.handlers.SysLogHandler(address='/dev/log')
-    syslogHandler.setFormatter(
-        logging.Formatter('%(filename)s[%(process)d]: [%(levelname)s] %(message)s'))
-
-    logger.addHandler(streamHandler)
-    logger.addHandler(syslogHandler)
+    syslog_handler = None
+    for addr in ['/dev/log', '/var/run/syslog']:
+        try:
+            syslog_handler = logging.handlers.SysLogHandler(address=addr)
+        except:
+            pass
+        else:
+            break
+    if syslog_handler:
+        syslog_handler.setFormatter(logging.Formatter('%(filename)s[%(process)d]: [%(levelname)s] %(message)s'))
+        logger.addHandler(syslog_handler)
 
     logger.setLevel(logging.DEBUG)
 
